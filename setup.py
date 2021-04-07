2e023c925fc63949885c9e9375c34aab22e9966e
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-04-07 17:07:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='reachy_pyluos_hal',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests']),
 
     install_requires=[
