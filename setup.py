cc15cbf13f135883716a6be160b810ac9177ee6c
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-01-31 17:16:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='reachy_pyluos_hal',
-    version='0.2.3',
+    version='0.3.0',
     packages=find_packages(exclude=['tests']),
 
     install_requires=[
