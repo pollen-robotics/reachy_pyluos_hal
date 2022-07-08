abc395118d82bedf9fbcf278da751e0b25bd41da
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-07-08 15:43:55 +0200

    Mobile base integration (#37)
    - Add script to get the mobile base version
    - Connect to Reachy's gates using udev rules on '/dev/gates*', if the udev is defined.

--- reachy_pyluos_hal/reachy.py
+++ reachy_pyluos_hal/reachy.py
@@ -26,7 +26,10 @@ class Reachy(GateProtocol):
     """Reachy wrapper around serial GateClients which handle the communication with the hardware."""
 
     if sys.platform == 'linux':
-        port_template: str = '/dev/ttyUSB*'
+        if len(glob('/dev/gate*')) == 0:
+            port_template: str = '/dev/ttyUSB*'
+        else:
+            port_template: str = '/dev/gate*'
     elif sys.platform == 'darwin':
         port_template: str = '/dev/tty.usbserial*'
     elif sys.platform == 'win32':
